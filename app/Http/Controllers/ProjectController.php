a10024cb8200429f57ee4546752a756943c83633
Ziel-Zone <azrielnoer28@gmail.com>
2025-10-25 17:40:32 +0700

    haha bisa oge

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,18 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
 class ProjectController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $user = auth::user();
+        // Ambil user yang sedang login dari $request
+        // Cara ini 100% "dikenali" oleh IDE dan PHP
+        $user = $request->user(); 
+        
+        // Ambil project milik user tersebut
+        // 'with('milestones')' adalah eager loading, sangat bagus
+        // FIX 2: 'projects' (plural), bukan 'project' (singular)
         $projects = $user->projects()->with('milestones')->get();
+
+        // Kirim data projects ke view 'projects.index'
         return view('projects.index', compact('projects'));
     }
 
